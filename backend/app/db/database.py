3ad856982eed458e04eecab510647ee0cf34d0c2
Waheeb Barqawi <trolltrolltrolllollollol@outlook.com>
2025-07-02 20:14:00 -0400

    fix: fix server issues when using psql

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -3,10 +3,9 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from app.config import DATABASE_URL
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
-
 
 def get_db():
     db = SessionLocal()
